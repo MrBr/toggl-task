7e5c21b33b1fc0f8c933bcbadfd66fa09e90bb2a
Luka Bracanovic <lbracano@gmail.com>
2022-11-10 14:15:03 +0100

    Implement sending emails and feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { FileInput, Files } from './components';
-import { useParseEmails } from './hooks';
+import { FileInput, Files, ApiFeedback } from './components';
+import { useParseEmails, useSendEmails } from './hooks';
 
 function App() {
   const [files, setFiles] = useState({});
-  const [parsedEmails] = useParseEmails(files);
+  const [parsedEmails, isParsing] = useParseEmails(files);
+  const [sendEmails, apiState, resetApiState] = useSendEmails();
+
+  const haveSelectedFiles = Object.keys(files).length > 0;
+
+  useEffect(() => {
+    if (!apiState.response || apiState.error) {
+      return;
+    }
+    setFiles({});
+  }, [apiState.response, apiState.error]);
+
+  useEffect(() => {
+    if (!haveSelectedFiles) {
+      return;
+    }
+    resetApiState();
+  }, [haveSelectedFiles, resetApiState, files]);
 
   const handleFiles = (selectedFiles) => {
     const newFiles = selectedFiles.reduce(
@@ -25,9 +42,21 @@ function App() {
     setFiles(newFiles);
   };
 
+  const handleSendEmails = async () => {
+    const uniqueEmailSet = new Set(Object.values(parsedEmails).flat());
+    const uniqueEmails = Array.from(uniqueEmailSet);
+    await sendEmails(uniqueEmails);
+  };
+
   return (
     <div className="app">
       <FileInput onFiles={handleFiles} />
+      <ApiFeedback apiState={apiState} />
+      <div className="app__actions">
+        <button disabled={!haveSelectedFiles && !isParsing} onClick={handleSendEmails}>
+          Send emails
+        </button>
+      </div>
       <Files files={files} onRemove={handleRemove}>
         {(fileName) => {
           const suffix = parsedEmails[fileName] ? parsedEmails[fileName].length : 'Parsing';
