a77ddc66ebbccfff9293d9bb3388f7b14c5135dc
Luka Bracanovic <lbracano@gmail.com>
2022-11-10 11:24:51 +0100

    Integrate FileInput and Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FileInput, Files } from './components';
 
 function App() {
-  return <div>Implement me!</div>;
+  const [files, setFiles] = useState({});
+
+  const handleFiles = (selectedFiles) => {
+    const newFiles = selectedFiles.reduce(
+      (res, file) => {
+        res[file.name] = file;
+        return res;
+      },
+      { ...files }
+    );
+
+    setFiles(newFiles);
+  };
+
+  const handleRemove = (fileName) => {
+    const newFiles = { ...files };
+    delete newFiles[fileName];
+    setFiles(newFiles);
+  };
+
+  return (
+    <div className="app">
+      <FileInput onFiles={handleFiles} />
+      <Files files={files} onRemove={handleRemove} />
+    </div>
+  );
 }
 
 export default App;
